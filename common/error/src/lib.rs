ecbcbd9345d8fe9149695fdb6b7b13c46645da6b
Yoshiki Matsuda <myskjp@gmail.com>
2022-07-25 09:19:40 +0900

    add NotFound

--- common/error/src/lib.rs
+++ common/error/src/lib.rs
@@ -2,8 +2,15 @@ use thiserror::Error;
 
 #[derive(Clone, Debug, Error)]
 pub enum AppError {
+    // 引数が不正である
     #[error("{0}")]
     InvalidArgument(String),
+
+    // エンティティが存在しない
+    #[error("{0}")]
+    NotFound(String),
+
+    // データベースへの接続失敗などの技術的問題
     #[error("{0}")]
     Internal(String),
 }
